46df10f0f33d4473be00bbedc80c292ae40931c6
nishkixs <nishon.radhakrishnan@gmail.com>
2016-09-07 16:38:14 -0400

    built grid and nav bar

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,22 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+
+$('#sign-in').on('click', function() {
+ $('#signInModal').modal('show');
+});
+
+$('#sign-in-button').on('click' ,function() {
+ $('#signInModal').modal('hide');
+ $('#sign-out').show();
+ $('#change-password').show();
+ $('#sign-up').hide();
+ $('#sign-in').hide();
+});
+
+$(document).ready(function() {
+ $('#sign-out').hide();
+ $('#change-password').hide();
+ $('#signInModal').modal('show');
+});
