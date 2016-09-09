855733193903094bf10ed2b2ff6be92bb7dacf43
nishkixs <nishon.radhakrishnan@gmail.com>
2016-09-08 21:41:22 -0400

    Win Conditions not working yet

--- assets/scripts/logic/events.js
+++ assets/scripts/logic/events.js
@@ -20,23 +20,60 @@ const onNewGame = function onNewGame(event) {
 let gameBoardArray = ['', '', '', '', '', '', '', '', ''];
 
 
-//let winCritera = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
 
-//  [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
 
-const onPlaceX = function (event) {
+const XOonBoard = function (event) {
+  let id=this.id;
   event.preventDefault();
   if (app.player === 'x') {
     app.player = 'o';
     let cellclicked = event.target;
     $(cellclicked).html('O');
+    //let index = $('#' + cellclicked).data("id");
+    gameBoardArray[id] = app.player;
+    console.log(gameBoardArray);
+
   } else if (app.player === 'o') {
     app.player = 'x';
     let cellclicked = event.target;
     $(cellclicked).html('X');
+
+    gameBoardArray[id] = app.player;
+    console.log(gameBoardArray);
+
+
   }
 };
 
+
+//if gameBoardArray ==
+
+
+// let winCriteria = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+//
+//  [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
+//
+//  // checks if move is possible
+//  const isValidMove = (id) => {
+//   return (!game.currentGame.cells[id] && !game.currentGame.over);
+//
+//  };
+
+const endOfGame = function(){
+
+  if (gameBoardArray[0]===gameBoardArray[1]&&gameBoardArray[0]===gameBoardArray[2]||
+    gameBoardArray[3]===gameBoardArray[4]&&gameBoardArray[3]===gameBoardArray[5]||
+    gameBoardArray[6]===gameBoardArray[7]&&gameBoardArray[6]===gameBoardArray[8]||
+    gameBoardArray[0]===gameBoardArray[3]&&gameBoardArray[0]===gameBoardArray[6]||
+    gameBoardArray[1]===gameBoardArray[4]&&gameBoardArray[1]===gameBoardArray[7]||
+    gameBoardArray[2]===gameBoardArray[5]&&gameBoardArray[2]===gameBoardArray[8]||
+    gameBoardArray[0]===gameBoardArray[4]&&gameBoardArray[0]===gameBoardArray[8]||
+    gameBoardArray[2]===gameBoardArray[4]&&gameBoardArray[2]===gameBoardArray[6]||
+);
+{console.log('someone won')}
+
+
+};
 
 
 
@@ -46,7 +83,7 @@ const onPlaceX = function (event) {
 const addHandlers = () => {
   $('.newgamebutton').on('click', onNewGame);
 
-  $('.col-xs-4').on('click', onPlaceX);
+  $('.col-xs-4').on('click', XOonBoard);
 };
 
 
@@ -57,6 +94,6 @@ const addHandlers = () => {
 
 module.exports = {
   addHandlers,
-  onPlaceX,
+  XOonBoard,
   gameBoardArray,
 };
